d2507bc80f7fd05f5d56234287481638c6f49506
sininen-blue <alaanneil@gmail.com>
2023-05-14 18:45:44 +0800

    reworked add task and added get_all_tasks

--- pytodo.py
+++ pytodo.py
@@ -4,21 +4,33 @@
 # reprioritize
 # deprioritize
 # list
+todo_file = "todo.txt"
+
+def get_all_tasks():
+    all_tasks = []
+    
+    with open(todo_file, "r") as file:
+        all_tasks = file.readlines()
+
+    return all_tasks
 
 def add_task(task, index = -1):
     if index == -1:
-        file = open("todo.txt", "a")
+        file = open(todo_file, "a")
         file.write(f"{task}\n")
         file.close()
     else:
-        all_tasks = []
+        all_tasks = get_all_tasks()
 
-        file = open("todo.txt", "r")
-        for item in file:
-            all_tasks.append(item)
-        file.close()
-       
         all_tasks.insert(index-1, f"{task}\n")
-        file = open("todo.txt", "w")
+        
+        file = open(todo_file, "w")
         file.writelines(all_tasks)
+        file.close()
+
+def done_task():
+    all_tasks = get_all_tasks()
+
+add_task("testing2")
+
 
